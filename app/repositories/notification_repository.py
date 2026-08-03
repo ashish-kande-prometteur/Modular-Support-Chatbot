697ec10a1a112cc4b78b49d3cda872cfa05a7bb8
ashish-kande-prometteur <ashish.k@prometteur>
2026-08-03 18:49:56 +0530

    feat: Implement notification read status management and fetch unread notifications

--- app/repositories/notification_repository.py
+++ app/repositories/notification_repository.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.notification_model import Notification
-
+from datetime import datetime , timezone
+from uuid import UUID
 class NotificationRepository:
 
     @staticmethod
@@ -24,5 +25,41 @@ class NotificationRepository:
         )
 
         db.add(notification)
+
+        return notification
+
+    @staticmethod
+    def get_unread_notifications(db: Session):
+
+        return (
+            db.query(Notification)
+            .filter(Notification.is_read == False)
+            .order_by(Notification.created_at.desc())
+            .all()
+        )
+
+    @staticmethod
+    def get_by_id(
+        db: Session,
+        notification_id: UUID
+    ):
+
+        return (
+            db.query(Notification)
+            .filter(Notification.id == notification_id)
+            .first()
+        )
+
+    @staticmethod
+    def mark_as_read(
+        db: Session,
+        notification: Notification
+    ):
+
+        notification.is_read = True
+        notification.read_at = datetime.now(timezone.utc)
+
+        db.commit()
+        db.refresh(notification)
 
         return notification
